518fef302ac2be87cda705ad44981072916ef3f3
Oren Vishny <ovishny@gmail.com>
2025-01-29 20:29:12 -0500

    Mongoose testing

--- mongo-api/server.js
+++ mongo-api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 // Initialize the app
 const app = express();
@@ -7,8 +8,9 @@ const app = express();
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+app.use(cors())
 // MongoDB connection URI (use the URI you copied from MongoDB Atlas)
-const uri = 'mongodb+srv://ovishny:Karatekid98@cluster0.mpf3mtc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const uri = 'mongodb+srv://ovishny:Karatekid98@cluster0.mpf3mtc.mongodb.net/TFT?retryWrites=true&w=majority&appName=Cluster0'
 
 // Connect to MongoDB
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -22,6 +24,7 @@ const DataSchema = new mongoose.Schema({
 });
 
 const DataModel = mongoose.model('Data', DataSchema);
+const Competitors = mongoose.model('Competitors', new mongoose.Schema({any:{}}), 'Competitors')
 
 // Example API route to fetch data from MongoDB
 app.get('/data', async (req, res) => {
@@ -32,6 +35,16 @@ app.get('/data', async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+app.get('/competitors', async (req, res) => {
+    try {
+      const data = await Competitors.find();
+      res.json(data);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+
 
 // Start the server
 const port = process.env.PORT || 5000;
